8231cf36691e8cdfc7f3added7f51ed5ca7801ac
hilarious3 <shahzad20022002@gmail.com>
2023-11-02 13:20:37 +0530

    path parameters

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path
 app=FastAPI()
 
+students={
+    1:{
+        "name":"shahzad",
+        "age":"21"
+    }
+}
 @app.get("/")
 def index():
     return{"name":"first Data"}
+@app.get("/get-student/{student_id}")
+def get_student(student_id:int): # int= Path(None,gt=0,lt=4)#gt is greater than.
+    return students[student_id]
