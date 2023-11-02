21f252a400b2e11ca9401a5f258f3d91e2966e9a
hilarious3 <shahzad20022002@gmail.com>
2023-11-02 14:24:44 +0530

    put method implementation

--- app.py
+++ app.py
@@ -17,6 +17,11 @@ class Studentvalue(BaseModel):
     age:int
     year:str
 
+class UpdateStudent(BaseModel):
+    name:Optional[str]=None
+    age:Optional[int]=None
+    year:Optional[str]=None
+
 @app.get("/")
 def index():
     return{"name":"first Data"}
@@ -37,5 +42,18 @@ def create_student(student_id:int,student:Studentvalue):
     if student_id in students:
         return {"Error":"student exists"}
     students[student_id]=student
+    return students[student_id]
+
+
+@app.put("/update-student/{student_id}")
+def update_student(student_id:int,student:UpdateStudent):
+    if student_id not in students:
+        return {"Error":"student does not exist"}
+    if student.name!=None:
+        students[student_id].name=student.name
+    if student.age!=None:
+        students[student_id].age=student.age
+    if student.year!=None:
+        students[student_id].year=student.year
     return students[student_id]
     
