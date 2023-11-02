3684de080cf3fcfb5c557140804b4d5e0a59923a
hilarious3 <shahzad20022002@gmail.com>
2023-11-02 13:39:09 +0530

    query parameter

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI,Path
+from typing import Optional
 app=FastAPI()
 
 students={
     1:{
         "name":"shahzad",
-        "age":"21"
+        "age":11
     }
 }
 @app.get("/")
@@ -13,3 +14,12 @@ def index():
 @app.get("/get-student/{student_id}")
 def get_student(student_id:int): # int= Path(None,gt=0,lt=4)#gt is greater than.
     return students[student_id]
+
+@app.get("/get-by-name")
+def get_student(*,name:Optional[str]=None,age:int= None):# none will remove the require parameter or simple we can write as name:str
+    for student_id in students:
+        if students[student_id]["name"]==name:
+            return students[student_id]
+    return {"Data":"Not Found"}
+
+
